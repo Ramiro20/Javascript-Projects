befaa612d44c62d4446d9d1be9c3c6d6932f2949
Ramiro20 <leomar20rambito@gmail.com>
2022-04-17 22:52:01 -0400

    class 3

---
+++ class3/arrays.js
@@ -0,0 +1,56 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/*console.log(arr.length);
+console.log(arr[4]);
+
+let sum = 0;
+/*for (let index = 0; index < arr.length; index++) {
+    let element = arr[index];
+    sum += element;
+}*/
+
+/*for (let index = 0; index < arr.length; index++) {
+    let element = arr[index];
+    if (typeof element != 'number') continue;
+    sum += element;
+}
+
+Array.prototype.saludo = 'Hola';*/
+
+console.log(arr.indexOf(2));
+
+// agregar elementos con
+// .push(el1,el2,el3) -> añade al final
+// .unshift(el1,el2,el3) -> añade al principio 
+
+console.log(arr.length);
+let newLength = arr.unshift('Javascript', 'desde', 'cero');
+console.log(arr);
+console.log("este es otro array");
+console.log(newLength);
+
+//eliminar un solo elemento (sin parametros)
+// .pop() -> elimina el ultimo elemento
+// shift() -> elimina el primer elemento
+
+let deletedEl = arr.pop();
+console.log(arr);
+console.log(deletedEl);
+
+// join() -> convierte un array en un string
+console.log(arr.join(' '));
+// splice() -> elimina elementos de un array
+arr.splice(2, 0, 'escuela', 'digital');
+console.log(arr);
+// slice() -> copia una parte de un array
+let arr2 = arr.slice(2, 5);
+console.log(arr2);
+
+// ES6
+// .find(cb);
+
+let arra = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let num = arra.find(el => el > 3);
+let numIndex = arra.findIndex(el => el > 3);
+console.log(num);
+console.log(numIndex);
